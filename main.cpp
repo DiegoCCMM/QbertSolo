3ca8b024d8abe81d42a7101906d9317ddea4de59
Diego Caballé <diegocaballe@hotmail.com>
2021-03-09 17:02:37 +0100

    resizable window

--- main.cpp
+++ main.cpp
@@ -14,10 +14,11 @@ enum Direction {
     TOPRIGHT, TOPLEFT, DOWNRIGHT, DOWNLEFT
 };
 
-struct Player {
+struct Character {
     float x;
     float y;
     Direction dir;
+    ALLEGRO_BITMAP *draw;
 };
 
 void must_init(bool test, const char *description) {
@@ -37,6 +38,7 @@ int main() {
     ALLEGRO_EVENT_QUEUE *queue = al_create_event_queue();
     must_init(queue, "queue");
 
+    al_set_new_display_flags(ALLEGRO_RESIZABLE);
     ALLEGRO_DISPLAY *disp = al_create_display(640, 480);
     must_init(disp, "display");
 
@@ -48,7 +50,8 @@ int main() {
     must_init(al_init_image_addon(), "image addon");
     ALLEGRO_BITMAP *player = al_load_bitmap("/home/diego/Desktop/info/4-2/Videojuegos/Qbert/qbert.png");
     must_init(player, "player");
-    Player player1{100, 100, DOWNRIGHT};
+    Character qbert{100, 100, DOWNRIGHT};
+    qbert.draw = player;
     int sourceX = 0, sourceY = 2;
 
     al_register_event_source(queue, al_get_keyboard_event_source());
@@ -62,9 +65,9 @@ int main() {
 
     source.push_back(0);
 
-    for (int i = 0; i < al_get_bitmap_width(player); i++) {
+    for (int i = 0; i < al_get_bitmap_width(qbert.draw); i++) {
         separatorColor = al_map_rgba(0, 0, 255, 255);
-        pixel = al_get_pixel(player, i, 0);
+        pixel = al_get_pixel(qbert.draw, i, 0);
 
         if (memcmp(&pixel, &lastPixel, sizeof(ALLEGRO_COLOR))) {
             if (!memcmp(&pixel, &separatorColor, sizeof(ALLEGRO_COLOR))) {
@@ -74,7 +77,7 @@ int main() {
                 else
                     width.push_back(i - width[width.size() - 1]);
             }
-        } else if (i == al_get_bitmap_width(player) - 1) {
+        } else if (i == al_get_bitmap_width(qbert.draw) - 1) {
             width.push_back(i - width[width.size() - 1]);
             lastPixel = pixel;
         }
@@ -84,10 +87,13 @@ int main() {
     int airTimer = 0;
     ALLEGRO_EVENT event;
 
+    //GAME LOOP
+
     al_start_timer(timer);
     while (!done) {
         al_wait_for_event(queue, &event);
         al_get_keyboard_state(&keyState);
+        al_acknowledge_resize(disp);
 
         switch (event.type) {
             case ALLEGRO_EVENT_TIMER:
@@ -96,20 +102,20 @@ int main() {
                     airTimer++;
                     if(airTimer < airTime/2){
                         //GO UP AND DIRECTION
-                        if(player1.dir == TOPRIGHT || player1.dir == DOWNRIGHT)
-                            player1.x += movementX;
+                        if(qbert.dir == TOPRIGHT || qbert.dir == DOWNRIGHT)
+                            qbert.x += movementX;
                         else
-                            player1.x -= movementX;
-                        if(player1.dir != DOWNRIGHT && player1.dir != DOWNLEFT)
-                            player1.y -= movementY;
+                            qbert.x -= movementX;
+                        if(qbert.dir != DOWNRIGHT && qbert.dir != DOWNLEFT)
+                            qbert.y -= movementY;
                     }else if (airTimer > airTime/2 && airTimer < airTime){
                         //GO DOWN AND DIRECTION
-                        if(player1.dir == TOPRIGHT || player1.dir == DOWNRIGHT)
-                            player1.x += movementX;
+                        if(qbert.dir == TOPRIGHT || qbert.dir == DOWNRIGHT)
+                            qbert.x += movementX;
                         else
-                            player1.x -= movementX;
-                        if(player1.dir == DOWNRIGHT || player1.dir == DOWNLEFT)
-                            player1.y += movementY;
+                            qbert.x -= movementX;
+                        if(qbert.dir == DOWNRIGHT || qbert.dir == DOWNLEFT)
+                            qbert.y += movementY;
                     }else if (airTimer > airTime) {
                         //WE LANDED
                         airTimer = 0;
@@ -123,16 +129,16 @@ int main() {
                 if (!jumping) {
                     switch (event.keyboard.keycode) {
                         case ALLEGRO_KEY_LEFT:
-                            player1.dir = TOPLEFT;
+                            qbert.dir = TOPLEFT;
                             break;
                         case ALLEGRO_KEY_RIGHT:
-                            player1.dir = DOWNRIGHT;
+                            qbert.dir = DOWNRIGHT;
                             break;
                         case ALLEGRO_KEY_UP:
-                            player1.dir = TOPRIGHT;
+                            qbert.dir = TOPRIGHT;
                             break;
                         case ALLEGRO_KEY_DOWN:
-                            player1.dir = DOWNLEFT;
+                            qbert.dir = DOWNLEFT;
                             break;
                     }
                     jumping = true;
@@ -143,13 +149,15 @@ int main() {
                 done = true;
                 break;
         }
-
+        //TODO CAMERA SCALING CLOSER
         if (redraw && al_is_event_queue_empty(queue)) {
             //REDRAW THE IMAGE WITH EVERYTHING
             al_clear_to_color(al_map_rgb(0, 0, 0));
+
             //TODO MAP FUNCTION OR SOMETHING
-            al_draw_bitmap_region(player, sourceX + (player1.dir * 2 * framePixels), 0, framePixels, framePixels,
-                                  player1.x, player1.y, 0);
+
+            al_draw_bitmap_region(qbert.draw, sourceX + (qbert.dir * 2 * framePixels), 0, framePixels, framePixels,
+                                  qbert.x, qbert.y, 0);
 
             al_flip_display();
 
@@ -157,7 +165,7 @@ int main() {
         }
     }
 
-    al_destroy_bitmap(player);
+    al_destroy_bitmap(qbert.draw);
     al_destroy_font(font);
     al_destroy_display(disp);
     al_destroy_timer(timer);
