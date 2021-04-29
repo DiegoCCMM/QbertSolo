58496d38b825bd3359f4e7133b9841e1c542391e
Diego Caballé <diegocaballe@hotmail.com>
2021-04-29 13:16:12 +0200

    slick y sam terminados

---
+++ SlickSam.hpp
@@ -0,0 +1,33 @@
+//
+// Created by diego on 29/4/21.
+//
+
+#ifndef ALLEGRO5TUTORIAL_SLICKSAM_HPP
+#define ALLEGRO5TUTORIAL_SLICKSAM_HPP
+
+class Coily : public Enemy{
+
+    State state = GOING_DOWN;
+    ALLEGRO_BITMAP* secondPhaseDraw;
+    int reachQberti = VACIO,  reachQbertj = VACIO;
+
+public:
+    SlickSam(const Piramide &piramide, const std::string &nom, int i, int j, int xRespectCube, int yRespectCube) : Enemy(
+            piramide, nom, i, j, xRespectCube, yRespectCube) {
+        setChangingGroundPower(true);
+    }
+
+    void randomMovement(int i, int j) override {
+
+
+
+    }
+
+    void drawBitmap() override {
+            al_draw_bitmap_region(getDraw(), getSourceX() + (getDir() * 2 * sizePixelsX),
+                                  getSourceY() + (sourceI * sizePixelsY), sizePixelsX, sizePixelsY*2,
+                                  getX(), getY(), 0);
+    }
+};
+
+#endif //ALLEGRO5TUTORIAL_SLICKSAM_HPP
